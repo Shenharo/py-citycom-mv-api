5d1e27fc5aa14202f45d85377cb664cb568ce980
Omer Shenhar <shenharo@gmail.com>
2024-03-19 09:53:05 +0200

    try ruff fix

--- citycom_mv_api/models/auth_error.py
+++ citycom_mv_api/models/auth_error.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass, field
 from mashumaro import DataClassDictMixin, field_options
 
 
-
 @dataclass
 class AuthError(DataClassDictMixin):
     error_type: str = field(metadata=field_options(alias="error"))
